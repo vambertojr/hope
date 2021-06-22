127a7689801ed5e61f3bf8642d5718358ea2cf13
vambertojr <vambertojr@gmail.com>
2021-06-22 10:35:50 -0300

    Ajuste de layout na exibição do texto da pergunta.

--- lib/visoes/responder_quiz.dart
+++ lib/visoes/responder_quiz.dart
@@ -116,13 +116,13 @@ class ResponderQuizState extends State<ResponderQuiz> {
     return Expanded(
       flex: 5,
       child: Padding(
-        padding: EdgeInsets.symmetric(vertical: 16.0),
+        padding: EdgeInsets.all(16.0),
         child: Center(
           child: Text(
             question,
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 25.0,
+              fontSize: 23.0,
               color: Colors.black,
             ),
           ),
